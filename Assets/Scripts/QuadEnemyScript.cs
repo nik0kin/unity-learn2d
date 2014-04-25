e074f80b163e5f6977d0053068f6812a1fbad87a
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-04-25 18:06:56 -0500

    a little more learn

---
+++ Assets/Scripts/QuadEnemyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enemy generic behavior
+/// </summary>
+public class QuadEnemyScript : MonoBehaviour
+{
+	private WeaponScript[] weaponScripts;
+	private Transform[] weaponTransforms;
+	
+	void Awake()
+	{
+		weaponTransforms = GetComponentsInChildren<Transform>();
+		// Retrieve the weapon only once
+		weaponScripts = GetComponentsInChildren<WeaponScript>();
+	}
+	
+	void Update()
+	{
+		// Get new weapon Z-axis rotation positions
+		// lazy way (every update)
+		foreach (Transform transform in weaponTransforms) {
+			transform.Rotate(0, 0, Random.Range(-180.0f, 180.0f));
+		}
+
+		foreach (WeaponScript weapon in weaponScripts)
+		{
+			// Auto-fire
+			if (weapon != null && weapon.CanAttack)
+			{
+				weapon.Attack(true);
+			}
+		}
+	}
+}
